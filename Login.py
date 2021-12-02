ee5c5f22089f41dbb3d5956499f8c06a2b2958ce
dreo <54491122+Andrei7871@users.noreply.github.com>
2021-12-02 10:52:26 +0800

    Update Login.py

--- Login.py
+++ Login.py
@@ -1,6 +1,7 @@
 import sys
 import mysql.connector
 import smtpd
+from time import sleep
 #--------------MYSQL------------------------------------
 mydb=mysql.connector.connect(
     host="localhost",
@@ -17,7 +18,9 @@ app=True
 status = ""
 
 def displayMenu():
+    print("################################################")
     status = input("WELCOME TO DJOYOFBAKING! \nAre you registered user? y/n? Press q to quit").replace(" ","")
+    print("################################################")
     if status == "y":
         oldUser()
     elif status == "n":
@@ -62,9 +65,47 @@ def oldUser():
     if results:
         for i in results:
             print("\nLogin successful!\n")
+            firstPage()
             break
     else:
         print("\nUser doesn't exist or wrong password!\n")
+def firstPage():
+    boolean1=True
+    print("#####################################\n")
+    print("[0]Search Item\n")
+    print("[1]View Cart\n")
+    print("[2]Add to Cart\n")
+    print("[3]Log out\n")
+    while boolean1==True:
+        user_input=input(":")
+        if(user_input=="0"):
+            print("Just Press Enter if you want to go back\nPlease type name of item you want to search\n")
+            user_input2=input(":")
+            if user_input2=="":
+                firstPage()
+            else:
+                print("Item not found!")
+                sleep(1.5)
+                firstPage()
+        elif(user_input=="1"):
+            print("Just Press Enter if you want to go back\nYour Cart:\n")
+            user_input3=input(":")
+            if user_input3=="":
+                firstPage()
+        elif(user_input=="2"):
+            print("Just Press Enter if you want to go back\nItem Name\tPrice")
+            print("Enzaimada\t30 Peses\n")
+            user_input4=input(":")
+            if user_input4=="":
+                firstPage()
+        elif user_input=="3":
+            boolean1 = False
+            print("Log out success")
+            displayMenu()
+        else:
+            firstPage()
+            print("Invalid Input!")
+
 
 while app==True:
     displayMenu()
