ea5dd3bbb3f2d17cd1ccc1019f188ee37d7193e7
dreo <54491122+Andrei7871@users.noreply.github.com>
2021-12-03 11:22:23 +0800

    IPTCFINALS.py
    I LOVE YOU ALL <3

--- Login.py
+++ Login.py
@@ -10,13 +10,9 @@ mydb=mysql.connector.connect(
     database="iptcfinals"
 )
 myc=mydb.cursor()
-
 #--------------MYSQL------------------------------------
-
-
 app=True
 status = ""
-
 def displayMenu():
     print("################################################")
     status = input("WELCOME TO DJOYOFBAKING! \nAre you registered user? y/n? Press q to quit").replace(" ","")
@@ -29,35 +25,60 @@ def displayMenu():
         app=False
         sys.exit("Exit Successfully")
 
-
 def newUser():
+    bool2=True
+    bool3=True
     gmail = "False"
-    addr = input(":").replace(" ", "")
-    createLogin = input("add Email Address: ").replace(" ","")
-    sql = "select * from customer where emailadd = %s "
-    myc.execute(sql, [(createLogin)])
-    results = myc.fetchall()
-    if "@gmail.com" in createLogin:
-        gmail = "True"
-    if results:
-        for i in results:
-            print("\nEmail Address already exist!\n")
-            break
-    else:
-        createPassw = input("Create password: ")
-        if gmail=="True":
-            sql = "INSERT INTO customer (address, emailadd, password) VALUES (%s,%s, %s)"
-            val = (addr, createLogin, createPassw)
-            myc.execute(sql, val)
-            mydb.commit()
-            print(myc.rowcount, "record inserted.")
-            print("\nUser created\n")
+    addr = input("Address:")
+    while bool2==True:
+        username=input("Username:")
+        sql2="select * from customer where username= %s"
+        myc.execute(sql2,[(username)])
+        result2=myc.fetchall()
+        if "@" in username or "#" in username or "$" in username or "*" in username or "%" in username or "&" in username or "|" in username or ";" in username or ":" in username or "[" in username or "]" in username or "'" in username:
+            print("Special characters are not allowed. Try again")
+            sleep(2)
+        else:
+            if result2:
+                for i in result2:
+                    print("Username already taken please try again")
+                    sleep(2)
+                    break
+            else:
+                bool2=False
+    while bool3==True:
+        createLogin = input("add Email Address: ").replace(" ","")
+        sql = "select * from customer where emailadd = %s "
+        myc.execute(sql, [(createLogin)])
+        results = myc.fetchall()
+        if "@gmail.com" in createLogin:
+            gmail = "True"
+            if results:
+                for i in results:
+                    print("\nEmail Address already exist! Please Try again\n")
+                    sleep(2)
+                    break
+            else:
+                bool3=False
         else:
             print("@gmail.com is required")
-
+            sleep(2)
+    createPassw = input("Create password: ")
+    if gmail=="True":
+        sql = "INSERT INTO customer (address, emailadd, password,username) VALUES (%s,%s, %s,%s)"
+        sql2="CREATE TABLE {0}(item varchar(250),price int,qty int)".format(username)
+        val = (addr, createLogin, createPassw,username)
+        myc.execute(sql2)
+        myc.execute(sql, val)
+        mydb.commit()
+        print(myc.rowcount, "record inserted.")
+        print("\nUser created\n")
+    else:
+        print("Something's Wrong. Please Try again")
+        sleep(2)
 
 def oldUser():
-    login = input("Enter login name: ").replace(" ","")
+    login = input("Enter Email Address: ").replace(" ","")
     passw = input("Enter password: ")
     sql="select * from customer where emailadd = %s and password = %s"
     myc.execute(sql,[(login),(passw)])
@@ -75,7 +96,7 @@ def firstPage():
     print("[0]Search Item\n")
     print("[1]View Cart\n")
     print("[2]Add to Cart\n")
-    print("[3]Log out\n")
+    print("[3]Log Out\n")
     while boolean1==True:
         user_input=input(":")
         if(user_input=="0"):
@@ -105,7 +126,6 @@ def firstPage():
         else:
             firstPage()
             print("Invalid Input!")
-
 
 while app==True:
     displayMenu()
